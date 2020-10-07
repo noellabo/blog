95d1eaa8e8515451d2bab87f6baa9ccbec8c385f
Takeshi Umeda <noel.yoshiba@gmail.com>
2020-10-08 00:47:49 +0900

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'noellabo\'s tech blog',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Qrunchがサービス終了したので、ひとまずこちらにお引っ越しです。',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
